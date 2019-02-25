7e44e7c07611ace2e00816b7e414505c1cad3aa0
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-25 19:48:46 -0300

    created car form

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,7 @@
 Rails.application.routes.draw do
 
-  get 'cars/index'
-  get 'cars/show'
-  get 'cars/new'
-  get 'cars/create'
-  get 'cars/edit'
-  get 'cars/update'
-  get 'cars/destroy'
-  root 'pages#home'  
+  root 'pages#home'
   devise_for :users
+  resources :cars
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
