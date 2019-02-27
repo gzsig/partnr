78915e5b879cdc9b1e7b1e0e60456a0d10c9ca17
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-27 15:54:37 -0300

    added profile pic to navbar but isnt round yet

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -2,4 +2,17 @@ class PhotoUploader < CarrierWave::Uploader::Base
   include Cloudinary::CarrierWave
 
   # Remove everything else
+
+  process eager: true  # Force version generation at upload time.
+
+process convert: 'jpg'
+
+version :thumnail do
+  resize_to_fit 150, 150
+end
+
+version :bright_face do
+  cloudinary_transformation effect: "brightness:5", radius: 20,
+    width: 150, height: 150, crop: :thumb, gravity: :face
+end
 end
