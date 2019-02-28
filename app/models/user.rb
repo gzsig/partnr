e8932f76eea11354e39efeedd433b5c75b50218c
Gabriel Zsigmond <gaazsig@gmail.com>
2019-02-28 11:20:28 -0300

    need to merge with mastr

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+
+  after_create :send_welcome_email
+
   devise :database_authenticatable, :registerable,
   :recoverable, :rememberable, :validatable
 
@@ -16,5 +19,11 @@ class User < ApplicationRecord
   validates :occupation, presence: true
 
   mount_uploader :profile_picture, PhotoUploader
+
+  private
+
+  def send_welcome_email
+    UserMailer.welcome(self).deliver_now
+  end
 
 end
