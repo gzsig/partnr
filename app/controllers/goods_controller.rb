9df34adbd17066ce05ac58202f4effa75dcfae41
Diego de Souza <diegogonzalesdesouza@gmail.com>
2019-03-01 12:54:35 -0300

    Done with filters

--- app/controllers/goods_controller.rb
+++ app/controllers/goods_controller.rb
@@ -4,14 +4,13 @@ class GoodsController < ApplicationController
 
   def index
     @fabrication_year_range = (Good.oldest_good_fabrication_year..Time.now.year + 1).to_a
-    @good_types = Good.good_types
+    @good_types = Good.good_types.map(&:capitalize)
     @min_price = Good.min_good_price.to_i
-    @max_price = Good.max_good_price.to_i
+    @max_price = Good.max_good_price.to_i + 100_000
     @colors = %w[Perola Branco Preto Prata Chumbo Vermelho Amarelo]
     @body_styles = %w[Hatchback Sedan SUV Conversivel Coupe]
 
     query = params[:query]
-    # brand = params[:brand]
     good_type = params[:good_type]
     fabrication_year = params[:fabrication_year]
     price = params["/goods"][:price] unless params["/goods"].nil?
@@ -21,9 +20,8 @@ class GoodsController < ApplicationController
     @goods = Good.all
     @goods = @goods.search_by_brand_and_model(query) unless !query.present?
     @goods = @goods.where(good_type: good_type) unless !good_type.present?
-    # @goods = @goods.where(brand: brand) unless !brand.empty?
     @goods = @goods.where('fabrication_year >= ?', fabrication_year) unless !fabrication_year.present?
-    @goods = @goods.where('price > ?', price) unless !price.present?
+    @goods = @goods.where('price >= ?', price) unless !price.present?
     @goods = @goods.where(color: color) unless !color.present?
     @goods = @goods.where(body_style: body_style) unless !body_style.present?
   end
