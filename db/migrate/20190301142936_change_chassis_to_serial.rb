344db2759db4e1a05be0d30259652bc3ba799a42
Diego de Souza <diegogonzalesdesouza@gmail.com>
2019-03-01 15:09:31 -0300

    solved migration bug

--- db/migrate/20190301142936_change_chassis_to_serial.rb
+++ db/migrate/20190301142936_change_chassis_to_serial.rb
@@ -1,5 +1,5 @@
-class Changeserial_numberToSerial < ActiveRecord::Migration[5.2]
+class ChangeChassisToSerial < ActiveRecord::Migration[5.2]
   def change
-    rename_column :goods, :serial_number, :serial_number
+    rename_column :goods, :chassis, :serial_number
   end
 end
