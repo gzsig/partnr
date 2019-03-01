995be19b35a3aabe4684a72dd2ffc0764ea9f7ca
Diego de Souza <diegogonzalesdesouza@gmail.com>
2019-03-01 18:51:50 -0300

    ssl to force - true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'partnrme.herokuapp.com' }
-  # or your custom domain name eg. "www.yourdomain.com"
 
   # Verifies that versions and hashed value of the package contents in the project's package.json
   config.webpacker.check_yarn_integrity = false
@@ -54,7 +53,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
